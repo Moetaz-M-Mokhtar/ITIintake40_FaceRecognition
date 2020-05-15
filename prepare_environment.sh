0b538d63811c5805bc0e193cc4fa8baec4b3943c
Moetaz Mohamed Mokhtar <moetazm.mokhtar@gmail.com>
2020-05-15 14:54:33 +0200

    -Adding docker files to enable gpu usage -Fixing apt configurations during docker build

--- prepare_environment.sh
+++ prepare_environment.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+if [ $# != 1 ]; then
+    echo "You need to specify whether are you going to use cpu or gpu for build"
+else
+    echo "Inflating scripts"
+    if [ "$1" == gpu ]; then
+        cp ./detection/docker/Dockerfile_gpu ./detection/Dockerfile
+        cp ./detection/docker/model_handler_gpu.py ./detection/models/detection/retinaface-R50/model_handler.py
+        cp ./detection/docker/requirements_gpu.txt ./detection/requirements.txt
+        cp ./recognition/docker/Dockerfile_gpu ./recognition/Dockerfile
+        cp ./recognition/docker/requirements_gpu.txt ./recognition/requirements.txt
+    elif [ "$1" == cpu ]; then
+        cp ./detection/docker/Dockerfile_cpu ./detection/Dockerfile
+        cp ./detection/docker/model_handler_cpu.py ./detection/models/detection/retinaface-R50/model_handler.py
+        cp ./detection/docker/requirements_cpu.txt ./detection/requirements.txt
+        cp ./recognition/docker/Dockerfile_cpu ./recognition/Dockerfile
+        cp ./recognition/docker/requirements_cpu.txt ./recognition/requirements.txt
+    else
+        echo "argument can be \'cpu\' or \'gpu\' only"
+    fi
+fi
+
 gdown -O  20180402-114759.zip --no-cookies https://drive.google.com/uc?id=1EXPBSXwTaqrSC0OhUdXNmKSh9qJUQ55-&export=download
 for job in `jobs -p`
 do
